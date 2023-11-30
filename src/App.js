c0dbe2294770da588185cc89fa3369a7f3bc8be5
Neethu <neethurameesh2@gmail.com>
2023-11-30 13:09:56 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,64 +1,73 @@
 
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
-import { PieChart, Pie, Tooltip, ResponsiveContainer, BarChart, XAxis, YAxis, Legend, CartesianGrid, Bar } from 'recharts';
+import { PieChart, Pie, Tooltip, ResponsiveContainer, BarChart, XAxis, YAxis, Legend, CartesianGrid, Bar,  Brush } from 'recharts';
 function App() {
-  const data=[
-    {name:"Facebook",value:200000000},
-    {name:"Instagram",value:150000000},
-    {name:"Twitter",value:100000000},
-    {name:"Telegram",value:200000000},
-    {name:"Youtube",value:200000000},
-    
-  ]
+  const [state,setState]=useState()
+  const result = [];
+  useEffect(()=>{
+axios.get('https://fakestoreapi.com/products').then(res=>{
+  console.log(res.data);
+  setState(res.data)
+ 
+ 
+})
+
+  },[])
+  const data = state?.map(({ price, title }) => ({ price, title }));
+
+console.log(data);
+
   return (
    <>
   
     
 <div>
-<h1>Pie chart</h1>
+<h1 style={{textAlign:"center"}}>Products</h1>
 
-
-
-
-        <PieChart width={400} height={400}>
-          <Pie
-            dataKey="value"
-            isAnimationActive={false}
-            data={data}
-            cx="50%"
-            cy="50%"
-            outerRadius={80}
-            fill="#8884d8"
-            label
-          />
-          
-          <Tooltip />
-        </PieChart>
+         <ResponsiveContainer width="100%" height={400}>
+          <PieChart>
+          <Brush dataKey="title" height={30} />
+            <Pie
+              dataKey="price"
+              isAnimationActive={false}
+              data={data}
+              cx="50%"
+              cy="50%"
+              outerRadius={80}
+              fill="#8884d8"
+              label
+            />
+            <Tooltip formatter={(value, name, props) => [`${props.payload.title}: $${value}`, 'Price']} />
+          </PieChart>
+        </ResponsiveContainer>
         </div>
 <div>
 
-  <h1>Barchart</h1>
+  
+ 
 
-  <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-          barSize={20}
-        >
-          <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Bar dataKey="value" fill="#8884d8" background={{ fill: '#eee' }} />
-        </BarChart>
-
+<ResponsiveContainer width="100%" height={400}>
+          <BarChart
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+            barSize={20}
+          >
+            <Brush dataKey="title" height={30} />
+            <XAxis dataKey="title" scale="point" padding={{ left: 10, right: 10 }} />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Bar dataKey="price" fill="#8884d8" background={{ fill: '#eee' }} />
+          </BarChart>
+        </ResponsiveContainer>
 
 </div>
 
